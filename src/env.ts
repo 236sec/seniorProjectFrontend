353a677e9ca2286d5bbf4accfdc9b360b2ef4df7
236sec <popole159za@gmail.com>
2025-12-11 19:17:48 +0700

    Add Frontend URL ENV

--- src/env.ts
+++ src/env.ts
@@ -12,6 +12,7 @@ export const env = createEnv({
   },
   client: {
     NEXT_PUBLIC_VERCEL_URL: z.string().min(1),
+    NEXT_PUBLIC_FRONTEND_URL: z.string().min(1),
   },
   // If you're using Next.js < 13.4.4, you'll need to specify the runtimeEnv manually
   runtimeEnv: {
@@ -22,6 +23,7 @@ export const env = createEnv({
     COINGECKO_DEMO_API_KEY: process.env.COINGECKO_DEMO_API_KEY,
     BACKEND_URL: process.env.BACKEND_URL,
     NEXT_PUBLIC_VERCEL_URL: process.env.NEXT_PUBLIC_VERCEL_URL,
+    NEXT_PUBLIC_FRONTEND_URL: process.env.NEXT_PUBLIC_FRONTEND_URL,
   },
 });
 
@@ -30,6 +32,9 @@ export const env = createEnv({
  * Handles cases where NEXT_PUBLIC_VERCEL_URL may or may not include protocol.
  */
 export function getBaseUrl(): string {
+  if (env.NEXT_PUBLIC_FRONTEND_URL) {
+    return env.NEXT_PUBLIC_FRONTEND_URL;
+  }
   const baseUrl = env.NEXT_PUBLIC_VERCEL_URL;
 
   // If already has protocol, return as-is
